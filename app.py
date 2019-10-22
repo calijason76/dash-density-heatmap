a700a79d6e95d968c3f2b33ab272d7ab40ce78ec
Jason Brant <54822381+calijason76@users.noreply.github.com>
2019-10-22 18:18:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ import pickle
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='dc_houses'
+app.title='DC Crime'
 
 ########## Define the data
-df = pd.read_csv('resources/DC_Properties.csv', index_col='Unnamed: 0')
+df = pd.read_csv('resources/Crime_Incidents_in_2019.csv', index_col='Unnamed: 0')
 df=df[df['PRICE'].between(300000, 500000)] # artificially reduce the number of data points for efficiency
 
 ########## Define the figure
 
-fig = go.Figure(go.Densitymapbox(lat=df['LATITUDE'], lon=df['LONGITUDE'], z=df['PRICE'], radius=10))
+fig = go.Figure(go.Densitymapbox(lat=df['LATITUDE'], lon=df['LONGITUDE'], z=df['NEIGHBORHOOD'], radius=10))
 fig.update_layout(mapbox_style="stamen-terrain",
                   mapbox_center_lon=-77.07,
                   mapbox_center_lat=38.92,
@@ -32,7 +32,7 @@ app.layout = html.Div(children=[
     html.H1('DC Houses'),
     html.Div([
         dcc.Graph(id='figure-1', figure=fig),
-        html.A('Code on Github', href='https://github.com/austinlasseter/dash-density-heatmap'),
+        html.A('Code on Github', href='https://github.com/calijason76/dash-density-heatmap'),
         html.Br(),
         html.A('Source:', href='https://plot.ly/python/mapbox-density-heatmaps')
     ])
